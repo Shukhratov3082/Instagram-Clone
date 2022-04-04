f551b91b12e44edb0051fb5df5ea1f50c9bf16da
Sorrybro802 <poweshuxratov@gmail.com>
2022-04-04 17:21:14 +0500

    test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Signup from "./containers/Signup";
 function App() {
   return (
     <Routes>
-      <Route path="/SignUp" element={<Signup/>}/>
-      <Route path="/Login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/login" element={<Login/>}/>
       <Route path="/" element={<Posts/>}/>
     </Routes>
 
--- src/containers/Signup.jsx
+++ src/containers/Signup.jsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
-
+import InstagramIcon from '../assets/Instagram Logo.svg'
+import Avatar from '../assets/avatar1.png'
 const Signup = () => {
     return (
         <Wrapper>
             <Container>
-
+                <img className='instagram' src={InstagramIcon} alt="" />
+                <img src={Avatar} alt="" />
+                <span>jacob_w</span>
+                <button>Login</button>
+                <span>Switch accounts</span>
+                
             </Container>
         </Wrapper>
     );
@@ -24,5 +30,21 @@ const Container = styled.div`
     width: 375px;
     height: 812px;
     background-color: white;
-    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    .instagram{
+        margin-bottom: 52px;
+    }
+    button{
+        width: 307px;        
+        height: 44px;
+        border-radius: 5px;
+        border: none;
+        color: white;
+        margin: 12px 0 30px 0;
+        background: rgba(55, 151, 239, 1);
+
+    }
 `
