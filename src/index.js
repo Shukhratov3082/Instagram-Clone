dd1c2b61eb127bb190704a97d1dc4ef8e4708eae
MuhammadjonAzimov <muhammadazimov921@gmail.com>
2022-04-07 13:46:24 +0500

    ozgartirish qoshildi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import './index.css';
-import React from "react";
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
